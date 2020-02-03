770f3c1b86fbd834e02827e65b42ca40e6f64e6d
Ode Dahay <odedahay@gmail.com>
2020-02-04 02:33:59 +0800

    updated settings

--- btre/settings.py
+++ btre/settings.py
@@ -145,6 +145,6 @@ MESSAGE_TAGS = {
 # Email config
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 465
-# EMAIL_HOST_USER = 'odedahay@gmail.com'
+# EMAIL_HOST_USER = ''
 # EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS = True
